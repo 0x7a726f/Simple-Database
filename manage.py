e4dc8957ed80fabc72ddd2280cc2442f35cf76af
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2021-01-11 23:14:18 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,20 @@
 import sqlite3
 def manage():
-    databaseName = str(input("Type database file name. e.g. database.db :\n"))
+    databaseName = str(input("Type database file name. e.g. database.db >>"))
     connection = sqlite3.connect(databaseName)
     cursor = connection.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tableName = str(cursor.fetchall())
     remove = ["[","]","(",")",",","'"," "]
+    remove2 = ["[","]","(",")","'","O"]
     tableName = ''.join(i for i in tableName if not i in remove)
-    print("Table :",tableName)
-    userChoice = str(input("Add data (1) | Edit data (2)"))
+    print("-----------------------------------------------------\nTable :",tableName,"")
+    cursorExecuteColumnName = "SELECT * FROM " + tableName
+    cursor.execute(cursorExecuteColumnName)
+    columnList = [member[0] for member in cursor.description]
+    columnShow = ' , '.join(r for r in columnList if not r in remove2)
+    print("Columns :",columnShow,"\n-----------------------------------------------------")
+    userChoice = str(input("Add data (1)    |    Edit data (2) >>"))
     if userChoice == "1":
         print()
 manage()
