db2cf9b3107e9e54ac68c6e2b28f753610ef2e15
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2021-01-13 15:43:56 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 def manage():
     databaseName = str(input("Type database file name. e.g. database.db >>"))
     connection = sqlite3.connect(databaseName)
@@ -12,9 +13,26 @@ def manage():
     cursorExecuteColumnName = "SELECT * FROM " + tableName
     cursor.execute(cursorExecuteColumnName)
     columnList = [member[0] for member in cursor.description]
+    columnNumber = len(columnList)
     columnShow = ' , '.join(r for r in columnList if not r in remove2)
     print("Columns :",columnShow,"\n-----------------------------------------------------")
     userChoice = str(input("Add data (1)    |    Edit data (2) >>"))
     if userChoice == "1":
-        print()
+        r = 1
+        addList = ["0"]
+        for i in range(0,(columnNumber-1)):
+            dataText = "Add data in table : " + columnList[r] + " >>"
+            addData = str(input(dataText))
+            addList.append(addData)
+            r = r + 1
+        addListTuple = tuple(addList)
+        addListTuple = ' , '.join(addListTuple)
+        print(addListTuple)
+        executeAdd = "INSERT INTO " + tableName + " VALUES ( " + addListTuple + " )"
+        cursor.execute(executeAdd)
+        connection.commit()
+        time.sleep(100)
+    else:
+        print("")
+        
 manage()
