301b7ab1a5536ec474171642538d9effb24053a5
0x7a726f <69540160+0x7a726f@users.noreply.github.com>
2021-01-25 22:30:57 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -15,7 +15,7 @@ def manage():
             remove = ["[","]","(",")",",","'"," "]
             remove2 = ["[","]","(",")","'",]
             tableName = ''.join(i for i in tableName if not i in remove)
-            print("-----------------------------------------------------\nTable :",tableName,"")
+            print("=====================================================\nTable :",tableName,"")
             cursorExecuteColumnName = "SELECT * FROM " + tableName
             cursor.execute(cursorExecuteColumnName)
             global columnList
@@ -24,7 +24,7 @@ def manage():
             columnNumber = len(columnList)
             global columnShow
             columnShow = ' , '.join(r for r in columnList if not r in remove2)
-            print("Columns :",columnShow,"\n-----------------------------------------------------")
+            print("Columns :",columnShow,"\n=====================================================")
         except:
             print("Error.Try again.")
             databaseEnter()
@@ -64,59 +64,88 @@ def manage():
             databaseEnter()
 
     def editData():
-        print("-----------------------------------------------------\nColumns :",columnShow,)
-        print("Select two columns to search from.")
-        firstColumn = str(input("1st column >>"))
-        secondColumn = str(input("2nd column >>"))
-        a = "Type " + firstColumn + " value >>"
-        firstColumnData = str(input(a))
-        b = "Type " + secondColumn + " value >>"
-        secondColumnData = str(input(b))
-        columnSet = set(columnList)
-        if firstColumn and secondColumn in columnSet:
-            try:
-                selectExecute = "SELECT * FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
-                cursor.execute(selectExecute)
-                selectedRow = cursor.fetchall()
-                selectedRow = list(sum(selectedRow, ()))
-                s = 0
-                for _ in range(0,columnNumber):
-                    print(columnList[s],": ",selectedRow[s])
-                    s = s + 1
-            except:
-                print("Data does not exist in these columns.Try again.")
-                editData()
-            l = 0
-
-            for _ in range(0,columnNumber):
-                a = "Change data in column " + columnList[l] + " (y/n) >>"
-                changeDataInput = str(input(a)) 
-                if changeDataInput == "y" or changeDataInput == "Y":
-                    changeData = str(input("Type data >>"))
-                    changeDataExecute = "UPDATE " + tableName + " SET " + columnList[l] + "=" + changeData + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
-                    cursor.execute(changeDataExecute)
-                    connection.commit()
-                    
-                    firstColumnDataExec = "SELECT " + firstColumn + " FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
-                    cursor.execute(firstColumnDataExec)
-                    firstColumnData = str(cursor.fetchall())
-                    print(type(firstColumnData))     
-                    firstColumnData = ''.join( _ for _ in firstColumnData if _ not in "[(')],")
-                    print(firstColumnData)
-
-                    secondColumnDataExec = "SELECT " + secondColumn + " FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
-                    cursor.execute(secondColumnDataExec)
-                    secondColumnData = str(cursor.fetchall())
-                    print(type(secondColumnData))
-                    secondColumnData = ''.join( _ for _ in secondColumnData if _ not in "[(')],")
-                    print(secondColumnData)
-
+        try:
+            print("=====================================================\nColumns :",columnShow,)
+            print("Select two columns to search from.")
+            firstColumn = str(input("1st column >>"))
+            secondColumn = str(input("2nd column >>"))
+            a = "Type " + firstColumn + " value >>"
+            firstColumnData = str(input(a))
+            b = "Type " + secondColumn + " value >>"
+            secondColumnData = str(input(b))
+            columnSet = set(columnList)
+            if firstColumn and secondColumn in columnSet:
+                try:
+                    selectExecute = "SELECT * FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                    cursor.execute(selectExecute)
+                    selectedRow = cursor.fetchall()
+                    selectedRow = list(sum(selectedRow, ()))
+                    s = 0
+                    for _ in range(0,columnNumber):
+                        print(columnList[s],": ",selectedRow[s])
+                        s = s + 1
+                except:
+                    print("Data does not exist in these columns.Try again.")
+                    editData()
+                l = 0
+                userInput = str(input("Edit data (1)    |    Delete row (2) >>"))
+                if userInput == "1":
+                    for _ in range(0,columnNumber):
+                        a = "Change data in column " + columnList[l] + " (y/n) >>"
+                        changeDataInput = str(input(a)) 
+                        if changeDataInput == "y" or changeDataInput == "Y":
+                            if columnList[l] == firstColumn:
+                                changeData = str(input("Type data >>"))
+                                changeDataExecute = "UPDATE " + tableName + " SET " + firstColumn + '="' + changeData + '" WHERE ' + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                                print(changeDataExecute)
+                                cursor.execute(changeDataExecute)
+                                connection.commit()
+                                firstColumnDataExec = "SELECT " + firstColumn + " FROM " + tableName + " WHERE " + firstColumn + '="' + changeData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                                cursor.execute(firstColumnDataExec)
+                                firstColumnData = str(cursor.fetchall())
+                                firstColumnData = ''.join( _ for _ in firstColumnData if _ not in "[(')],")
+                                print(firstColumnData)   
+                            elif columnList[l] == secondColumn:
+                                changeData = str(input("Type data >>"))
+                                changeDataExecute = "UPDATE " + tableName + " SET " + secondColumn + '="' + changeData + '" WHERE ' + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                                cursor.execute(changeDataExecute)
+                                connection.commit()
+                                secondColumnDataExec = "SELECT " + secondColumn + " FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + changeData + '"'
+                                cursor.execute(secondColumnDataExec)
+                                secondColumnData = str(cursor.fetchall())
+                                secondColumnData = ''.join( _ for _ in secondColumnData if _ not in "[(')],")
+                                print(secondColumnData)
+                            else:
+                                changeData = str(input("Type data >>"))
+                                changeDataExecute = "UPDATE " + tableName + " SET " + columnList[l] + "=" + changeData + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                                cursor.execute(changeDataExecute)
+                                connection.commit()
+                        else:
+                            pass
+                        l = l + 1
+                    print("Edits successfully completed.")
+                    userChoiceAsk()
+                elif userInput == "2":
+                    userInput2 = str(input("Are you sure? (y/n) >>"))
+                    if userInput2 == "y" or userInput2 == "Y":
+                        deleteExecute = "DELETE FROM " + tableName + " WHERE " + firstColumn + '="' + firstColumnData + '" AND ' + secondColumn + '="' + secondColumnData + '"'
+                        cursor.execute(deleteExecute)
+                        connection.commit()
+                        print("Row deleted.")
+                        editData()
+                    elif userInput2 == "n" or userInput2 == "N":
+                        editData()
+                    else:
+                        print("Error.Try again.")
+                        editData()
                 else:
-                    pass
-                l = l + 1
-
-        else:
-            print("Columns do not exist in the table.Try again.")
+                    print("Error.Try again.")
+                    editData()
+            else:
+                print("Columns do not exist in the table.Try again.")
+                editData()
+        except:
+            print("Error.Try again.")
             editData()
 
     databaseEnter()
